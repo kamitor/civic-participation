716e509c549866e15d482a944222a64e72792a50
Jack Tanner <jack@jackandtheblockstalk.com>
2020-09-01 21:22:02 +0200

    updated settings

--- front-end/src/settings.js
+++ front-end/src/settings.js
@@ -9,14 +9,20 @@ let settings = {
         secure: false,
         network: "localhost:4000"
     },
+    env: 'development'
 };
 
 if (process.env.REACT_APP_NODE_ENV === "production") {
+    settings.env = "production"
     settings.eosio.nodeos = "https://d1hxgr8mqh915l.cloudfront.net";
     settings.dfuseOptions.network = "";
     settings.eosio.blockExplorerUrl = "d1uzqj8k54wt9u.cloudfront.net";
     settings.dfuseOptions.network = "d1hxgr8mqh915l.cloudfront.net"
     settings.dfuseOptions.secure = true
+}
+
+settings.isLiveEnvironment = function() {
+    return settings.env === "production";
 }
 
 export default settings;
