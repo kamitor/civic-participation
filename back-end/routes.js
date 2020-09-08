11d1e27e53dbfcc4fe2793d5cff0ae2ad00cf8a5
Jack Tanner <jack@jackandtheblockstalk.com>
2020-09-08 19:57:36 +0200

    search_transactions extended endpoint start

--- back-end/routes.js
+++ back-end/routes.js
@@ -4,9 +4,11 @@ const asyncRouter = require('./middleware/asyncRouter');
 
 const newAccount = require('./routes/new-account');
 const getAccount = require('./routes/chain/get_account');
+const searchTransactions = require('./routes/chain/search_transactions');
 
 // Blockchain API extensions
 router.post("/v1/chain/get_account", asyncRouter(getAccount));
+router.get("/v0/search/transactions", asyncRouter(searchTransactions));
 
 // New API endpoints
 router.post('/login', asyncRouter(newAccount));
---
+++ back-end/routes/chain/search_transactions.js
@@ -0,0 +1,36 @@
+const Accountability = require('../../services/Accountability');
+const accountability = new Accountability();
+
+
+/* GET acounts listing. */
+module.exports = async function(req, res) {
+    const transactionRes = req.blockchainRes;
+    const transactionSet = transactionRes.transactions;
+
+    // Find the account name of the account that siged the transaction
+    let newTransactionSet = [];
+    if (transactionSet) {
+        newTransactionSet = transactionSet.filter(trx => trx.transaction_status === "executed" && trx.pub_keys);
+    }
+
+    await Promise.all(newTransactionSet.map((trx, index) => getAuth(newTransactionSet, trx, index)));
+
+
+    // Add the common name of the account that signed the transaction
+    // TODO
+
+    const retObj = req.addBlockchainRes(newTransactionSet);
+    res.send(retObj);
+};
+
+// adds 'auth' property to the tx based on the signing key
+async function getAuth(transactionArray, trx, index) {
+    const pubKey = trx.pub_keys[0];
+    const blockNum = trx.execution_trace.action_traces[0].block_num;
+
+    if (!pubKey) throw new Error("New public key found on trx at index" + index);
+
+    const keyRes = await accountability.dfuseClient.stateKeyAccounts(pubKey, { block_num: blockNum });
+    if (!keyRes || !keyRes.account_names) throw new Error(`Accounts for public key ${pubKey} could not be found at block height ${blockNum}`)
+    transactionArray[index].auth = keyRes.account_names[0];
+}
