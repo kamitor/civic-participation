364c5423c601205a34dd90e4078cfec137ecf196
Jack Tanner <>
2020-09-25 18:34:08 +0200

    updated server details

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Creating shared environment variables"
-export SERVER_DOMAIN="ec2-18-132-210-99.eu-west-2.compute.amazonaws.com"
+export SERVER_DOMAIN="ec2-18-133-123-232.eu-west-2.compute.amazonaws.com"
 export SERVER_HTTPS_DOMAIN=""
--- scripts/local/install_software.sh
+++ scripts/local/install_software.sh
@@ -2,22 +2,25 @@
 
 # Ubuntu 18 machine
 
-# Docker
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install -y docker-ce
-sudo systemctl status docker
+# Docker and docker-compose
+sudo apt install docker docker-compose -y
+
+# # Docker
+# sudo apt update
+# sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+# sudo apt update
+# apt-cache policy docker-ce
+# sudo apt install -y docker-ce
+# sudo systemctl status docker
 
 sudo usermod -aG docker ${USER}
 
-# Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+# # Docker Compose
+# sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+# docker-compose --version
 
 # nvm with latest node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
