16e94d2bb4bc16ea675b2e630753fb1f46c23b52
Jack Tanner <>
2020-09-26 18:43:52 +0200

    removed eosio.token

--- contracts/eosio.token/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-WORKING_DIR="/tmp"
-CONTRACT_NAME="eosio.token"
-docker run -v "${PARENT_PATH}:${WORKING_DIR}" eostudio/eosio.cdt:v1.7.0 eosio-cpp -abigen -I ${WORKING_DIR}/include -R resource -contract ${CONTRACT_NAME} -o ${WORKING_DIR}/${CONTRACT_NAME}.wasm ${WORKING_DIR}/src/${CONTRACT_NAME}.cpp
