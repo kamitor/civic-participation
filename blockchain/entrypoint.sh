58820a17e415edfdddf796a3584254963b1a0bc4
Jack Tanner <>
2020-10-19 01:14:07 +0200

    set eosq api entrypoint

--- blockchain/entrypoint.sh
+++ blockchain/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo "Starting"
-echo "\"${REACT_APP_DFUSE_API_NETWORK}\""
-
 # cat dfuse.yaml
 # cat producer/config.ini
 
-REACT_APP_DFUSE_API_NETWORK=${REACT_APP_DFUSE_API_NETWORK} dfuseeos start \
+EOSQ_ENDPOINT_URL="localhost:8080"
+#EOSQ_ENDPOINT_URL="blockchain-civic.conscious-cities.com"
+
+set -x
+dfuseeos start \
     --data-dir /data \
     --search-common-dfuse-events-action-name=dfuseiohooks:event \
     --search-common-dfuse-events-unrestricted=true \
-    --common-chain-id df383d1cc33cbb9665538c604daac13706978566e17e5fd5f897eff68b88e1e4
+    --common-chain-id df383d1cc33cbb9665538c604daac13706978566e17e5fd5f897eff68b88e1e4 \
+    --eosq-api-endpoint-url=${EOSQ_ENDPOINT_URL}
