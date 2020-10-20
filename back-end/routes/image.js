3f0587793efdd3f670b89e3fccc3a094beb3e9b7
gangadharjannu <gangadhar014@gmail.com>
2020-10-20 23:28:29 +0200

    Changes imports type
    changes response format to object instead of string
    
    Improved indentation and formatting

--- back-end/routes/image.js
+++ back-end/routes/image.js
@@ -1,8 +1,7 @@
-import base64ToPNG from '../utils/base64ToPng';
-
-
 const fs = require('fs');
 const crypto = require('crypto')
+
+const base64ToPNG = require('../utils/base64ToPng');
 
 /**
  * Stores the proposal photo with sha256 as key and returns sha256 of the image.
@@ -10,17 +9,17 @@ const crypto = require('crypto')
  * @param {string} base64 string of an image.
  * @return {sha256} - sha256 hash of the image.
  */
-module.exports = async function(req, res) {
+module.exports = async function (req, res) {
     const image = req.body.photoString;
-    if(!image){
+    if (!image) {
         res.status(400).send('You should send image data to store get sha256 hash');
     }
     try {
         const imageSha256 = crypto.createHash("sha256").update(image).digest("hex");
         await base64ToPNG(image, imageSha256);
         // send sha256 of image
-        res.send(imageSha256);
-    } catch(e){
+        res.send({imageSha256});
+    } catch (e) {
         res.status(400).send(`Error creating image from base64 data, ${e}`);
     }
 }
