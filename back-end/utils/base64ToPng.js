9c36ed5380d081071f662a457dc37dadb0b1b4cd
gangadharjannu <gangadhar014@gmail.com>
2020-10-20 23:25:07 +0200

    TECHNICAL - Updates base64ToPng method
    Adds automatic extension for file
    
    converts es6 modules to require.js syntax

--- back-end/utils/base64ToPng.js
+++ back-end/utils/base64ToPng.js
@@ -1,4 +1,4 @@
-const settings = require('./settings');
+const settings = require('../settings');
 const fs = require('fs');
 const path = require('path');
 
@@ -7,22 +7,24 @@ const path = require('path');
  * @param {*} baseImage - base64 string of image
  * @param {*} fileName - filename of the image
  */
-export default function base64ToPNG(base64String, fileName) {
-   return new Promise((resolve, reject)=>{
+module.exports = async function base64ToPNG(base64String, fileName) {
+    return new Promise((resolve, reject) => {
         //path of folder where you want to save the image.
         const localPath = settings.imageUploadDirectory;
         //Check that if directory is present or not.
         if (!fs.existsSync(localPath)) {
             fs.mkdirSync(localPath);
         }
+
+        const ext = base64String.substring(base64String.indexOf("/") + 1, base64String.indexOf(";base64"));
+        
         const data = base64String.replace(/^data:image\/png;base64,/, '');
 
-        fs.writeFile(path.resolve(__dirname, `${localPath}${fileName}.png`), data, 'base64', function(err) {
+        fs.writeFile(path.resolve(__dirname, `${localPath}${fileName}.${ext}`), data, 'base64', function (err) {
             if (err) {
                 reject(err);
             };
             resolve('success');
         });
     });
-  
 }
