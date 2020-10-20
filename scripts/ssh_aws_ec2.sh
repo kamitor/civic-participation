0171f807c3d77bfa075fe83f97f0985ac1ad2b4a
Jack Tanner <>
2020-10-20 18:20:03 +0200

    deployment script

--- scripts/ssh_aws_ec2.sh
+++ scripts/ssh_aws_ec2.sh
@@ -2,3 +2,11 @@
 
 chmod 400 ./keys/ec2.pem
 ssh -i ./keys/ec2.pem ubuntu@ec2-18-133-123-109.eu-west-2.compute.amazonaws.com
+
+
+# cd civic-participation/
+# ./app.sh stop
+# git pull
+# ./app.sh install
+# ./app.sh start prod    OR
+# ./app.sh init prod            (if you need to reset blockchain or recompile contracts)
