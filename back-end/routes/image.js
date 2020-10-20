81a7cbfebb6516d54200fd1750158d204a2ede61
gangadharjannu <gangadhar014@gmail.com>
2020-10-20 19:05:32 +0200

    Code review comments: Implements async await instead of promises

--- back-end/routes/image.js
+++ back-end/routes/image.js
@@ -15,15 +15,11 @@ module.exports = async function(req, res) {
     if(!image){
         res.status(400).send('You should send image data to store get sha256 hash');
     }
-
     try {
         const imageSha256 = crypto.createHash("sha256").update(image).digest("hex");
-        base64ToPNG(image, imageSha256).then(response=>{
-            // send sha256 of image
-            res.send(imageSha256);
-        }).catch(error=>{
-            throw new Error(error)
-        });        
+        await base64ToPNG(image, imageSha256);
+        // send sha256 of image
+        res.send(imageSha256);
     } catch(e){
         res.status(400).send(`Error creating image from base64 data, ${e}`);
     }
