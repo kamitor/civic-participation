3a02899cedc2e5f9aaf711f64bd6f661d59d1e1b
Jack Tanner <>
2020-10-29 22:59:43 +0100

    categories type

--- front-end/src/types/proposals/categories.js
+++ front-end/src/types/proposals/categories.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import { mapObj } from '../../services/objects';
+import EcoIcon from '@material-ui/icons/Eco';
+import ChildCareIcon from '@material-ui/icons/ChildCare';
+
 const ProposalCategory = {
     Green: 0,
     Kids: 1,
-    Road: 2
+    Safety: 2,
+    Accessibility: 3,
+    Art: 4,
+    Health: 5,
+    Roads: 6,
+    Residential: 7
 }
 
 export default ProposalCategory;
@@ -9,16 +19,44 @@ export default ProposalCategory;
 const categoryReverse = {
     0: {
         label: "Green",
-        icon: "some icon code...???"
+        icon: EcoIcon
     },
     1: {
-        label: "Kids"
+        label: "Kids",
+        icon: ChildCareIcon
     },
     2: {
+        label: "Safety"
+    },
+    3: {
+        label: "Accessibility"
+    },
+    4: {
+        label: "Art"
+    },
+    5: {
+        label: "Health"
+    },
+    6: {
         label: "Road"
+    },
+    7: {
+        label: "Residential"
     }
 }
 
 export function toLabel(type) {
     return categoryReverse[type].label;
+}
+
+export function allCategories() {
+    const res = [];
+    mapObj(categoryReverse, (key, val) => {
+        res.push({
+            value: key,
+            label: val.label,
+            icon: val.icon
+        })
+    })
+    return res;
 }
