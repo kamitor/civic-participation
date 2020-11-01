84e114204c74ae88ece66139f9f6d67de716b92f
gangadharjannu <gangadhar014@gmail.com>
2020-11-01 11:15:50 +0100

    Adds middleware to intercept /v0/state/table blockchain call

--- back-end/routes.js
+++ back-end/routes.js
@@ -6,11 +6,14 @@ const login = require('./routes/login');
 const createAccount = require('./routes/create_account');
 const getAccount = require('./routes/chain/get_account');
 const searchTransactions = require('./routes/chain/search_transactions');
+const getTableData = require('./routes/chain/get_table_data');
 const storeImage = require('./routes/image');
 
 // Blockchain API extensions
 router.post("/v1/chain/get_account", asyncRouter(getAccount));
 router.get("/v0/search/transactions", asyncRouter(searchTransactions));
+// While getting proposal data
+router.get("/v0/state/table", asyncRouter(getTableData));
 
 // New API endpoints
 router.post('/login', asyncRouter(login));
