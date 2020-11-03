ea036939bfa51cfbbac1a7ebbaac27eaff04194a
gangadharjannu <gangadhar014@gmail.com>
2020-11-03 21:24:42 +0100

    Adds new route to intercept /v0/state/table/row blockchain request

--- back-end/routes.js
+++ back-end/routes.js
@@ -7,6 +7,7 @@ const createAccount = require('./routes/create_account');
 const getAccount = require('./routes/chain/get_account');
 const searchTransactions = require('./routes/chain/search_transactions');
 const getTableData = require('./routes/chain/get_table_data');
+const getTableRowData = require('./routes/chain/get_table_row_data');
 const storeImage = require('./routes/image');
 
 // Blockchain API extensions
@@ -14,6 +15,7 @@ router.post("/v1/chain/get_account", asyncRouter(getAccount));
 router.get("/v0/search/transactions", asyncRouter(searchTransactions));
 // While getting proposal data
 router.get("/v0/state/table", asyncRouter(getTableData));
+router.get("/v0/state/table/row", asyncRouter(getTableRowData));
 
 // New API endpoints
 router.post('/login', asyncRouter(login));
