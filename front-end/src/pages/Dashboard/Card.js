612bd1f809ba127ea3e7e442580fb3190dd3cc1c
JC Melati <github@jcmelati.com>
2020-11-12 18:12:27 -0300

    Show vote count badge on Dashboard (#32)
    * Show vote count badge on Dashboard when filtering by Approved to be voted
    
    * Add label to vote count
    
    Co-authored-by: JC Melati <hello@jcmelati.com>

--- front-end/src/pages/Dashboard/Card.js
+++ front-end/src/pages/Dashboard/Card.js
@@ -1,122 +1,165 @@
-import React from 'react'
-import { Grid, Typography, Button, Paper, ButtonBase } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import {
+  Badge,
+  Grid,
+  Typography,
+  Button,
+  Paper,
+  ButtonBase,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { withStyles } from "@material-ui/core/styles";
-import Logo from '../../assets/image/logo.png';
-
+import Logo from "../../assets/image/logo.png";
 
 const CardTitle = withStyles({
-    root: {
-        color: '#000000',
-        fontWeight: 'normal',
-        fontSize: '18px',
-        lineHeight: '21px',
-        '&:hover': {
-            cursor: 'pointer'
-        },
-        marginTop: 10
-    }
+  root: {
+    color: "#000000",
+    fontWeight: "normal",
+    fontSize: "18px",
+    lineHeight: "21px",
+    "&:hover": {
+      cursor: "pointer",
+    },
+    marginTop: 10,
+  },
 })(Typography);
 
 const CardContentDescription = withStyles({
-    root: {
-        color: '#000000',
-        fontWeight: 'normal',
-        fontSize: '10px',
-        marginTop: 10,
-        wordBreak: 'break-word'
-    }
+  root: {
+    color: "#000000",
+    fontWeight: "normal",
+    fontSize: "10px",
+    marginTop: 10,
+    wordBreak: "break-word",
+  },
 })(Typography);
 
 const ReadmoreButton = withStyles({
-    root: {
-        backgroundColor: '#1261A3',
-        borderRadius: 3,
-        border: 0,
-        color: 'white',
-        height: 36,
-        maxWidth: 96,
-        '&:hover': {
-            backgroundColor: '#1261A3'
-        }
+  root: {
+    backgroundColor: "#1261A3",
+    borderRadius: 3,
+    border: 0,
+    color: "white",
+    height: 36,
+    maxWidth: 96,
+    "&:hover": {
+      backgroundColor: "#1261A3",
     },
-    label: {
-        textTransform: 'capitalize',
-        fontSize: '12px',
-        fontWeight: 'normal'
-    },
+  },
+  label: {
+    textTransform: "capitalize",
+    fontSize: "12px",
+    fontWeight: "normal",
+  },
 })(Button);
 
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    right: 32,
+    top: 22,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "16px 7px",
+    fontSize: "22px",
+    zIndex: 900,
+    backgroundColor: "#599C6D",
+  },
+}))(Badge);
+
 export default function Card(props) {
-    const style = {
-        root: {
-            flexGrow: 1,
-        },
-        mainContainer: {
-            marginTop: 30
-        },
-        cardWrap: {
-            maxWidth: 800,
-            margin: 'auto'
-        },
-        paper: {
-            padding: 10,
-            margin: 'auto',
-            maxWidth: 285,
-            marginBottom: 30,
-            height: 300
-        },
-        image: {
-            width: 283,
-            height: 185
-        },
-        img: {
-            margin: 'auto',
-            display: 'block',
-            maxWidth: '100%',
-            maxHeight: '100%'
-        },
-        cardContent: {
-            padding: 10
-        },
-        cardTitle: {
-            minHeight: 50
-        }
-    }
-    if (props.selected) {
-        style.paper.backgroundColor = 'lightgrey'
-    }
-    const useStyles = makeStyles((theme) => (style))
+  const style = {
+    root: {
+      flexGrow: 1,
+    },
+    mainContainer: {
+      marginTop: 30,
+    },
+    cardWrap: {
+      maxWidth: 800,
+      margin: "auto",
+    },
+    paper: {
+      padding: 10,
+      margin: "auto",
+      maxWidth: 285,
+      marginBottom: 30,
+      height: 300,
+    },
+    image: {
+      width: 283,
+      height: 185,
+    },
+    img: {
+      margin: "auto",
+      display: "block",
+      maxWidth: "100%",
+      maxHeight: "100%",
+    },
+    cardContent: {
+      padding: 10,
+    },
+    cardTitle: {
+      minHeight: 50,
+    },
+  };
+  if (props.selected) {
+    style.paper.backgroundColor = "lightgrey";
+  }
+  const useStyles = makeStyles((theme) => style);
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Paper className={classes.paper}>
-            <Grid container direction="column">
-                <Grid item>
-                    <ButtonBase className={classes.image} onClick={props.onClick}>
-                        {props.imageUrl &&
-                            <img className={classes.img} alt={props.title} src={props.imageUrl} />
-                        }
-                        {!props.imageUrl &&
-                            <img className={classes.img} alt="Civic logo" style={{ opacity: 0.1 }} src={Logo} />
-                        }
-                    </ButtonBase>
-                </Grid>
-                <Grid item container className={classes.cardContent} direction="column">
-                    <Grid item container className={classes.cardTitle}>
-                        <Grid item xs>
-                            <CardTitle onClick={props.onClick}>{(props.title).slice(0, 30)}</CardTitle>
-                        </Grid>
-                        <ReadmoreButton onClick={props.onButtonClick}>READ MORE</ReadmoreButton>
-                    </Grid>
-                    <Grid item>
-                        <CardContentDescription>
-                            {(props.description).slice(0, 70)} ...
-                        </CardContentDescription>
-                    </Grid>
-                </Grid>
+  return (
+    <StyledBadge
+      invisible={!props.hasBadge}
+      badgeContent={props.badgeContent}
+      max={999}
+      color="error"
+    >
+      <Paper className={classes.paper}>
+        <Grid container direction="column">
+          <Grid item>
+            <ButtonBase className={classes.image} onClick={props.onClick}>
+              {props.imageUrl && (
+                <img
+                  className={classes.img}
+                  alt={props.title}
+                  src={props.imageUrl}
+                />
+              )}
+              {!props.imageUrl && (
+                <img
+                  className={classes.img}
+                  alt="Civic logo"
+                  style={{ opacity: 0.1 }}
+                  src={Logo}
+                />
+              )}
+            </ButtonBase>
+          </Grid>
+          <Grid
+            item
+            container
+            className={classes.cardContent}
+            direction="column"
+          >
+            <Grid item container className={classes.cardTitle}>
+              <Grid item xs>
+                <CardTitle onClick={props.onClick}>
+                  {props.title.slice(0, 30)}
+                </CardTitle>
+              </Grid>
+              <ReadmoreButton onClick={props.onButtonClick}>
+                READ MORE
+              </ReadmoreButton>
             </Grid>
-        </Paper>
-    );
+            <Grid item>
+              <CardContentDescription>
+                {props.description.slice(0, 70)} ...
+              </CardContentDescription>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </StyledBadge>
+  );
 }
--- front-end/src/pages/Dashboard/Dashboard.js
+++ front-end/src/pages/Dashboard/Dashboard.js
@@ -1,208 +1,248 @@
-import React, { useState, useEffect } from 'react'
-import { Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import NativeSelect from '@material-ui/core/NativeSelect';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import SearchIcon from '@material-ui/icons/Search';
-import Navbar from '../../components/Navbar/Navbar';
-import Card from './Card';
-import Map from './Map';
-import { ConsumeAuth } from '../../hooks/authContext';
+import React, { useState, useEffect } from "react";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import NativeSelect from "@material-ui/core/NativeSelect";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import SearchIcon from "@material-ui/icons/Search";
+import Navbar from "../../components/Navbar/Navbar";
+import Card from "./Card";
+import Map from "./Map";
+import { ConsumeAuth } from "../../hooks/authContext";
 import { useHistory } from "react-router-dom";
 import ProposalStatus from "../../types/proposals/status";
 
 function parseLocation(location) {
-    return {
-        lat: parseFloat(location.split(",")[0]),
-        lng: parseFloat(location.split(",")[1])
-    }
+  return {
+    lat: parseFloat(location.split(",")[0]),
+    lng: parseFloat(location.split(",")[1]),
+  };
 }
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
+  root: {
+    flexGrow: 1,
+  },
+  mainContainer: {
+    marginTop: 30,
+  },
+  cardWrap: {
+    maxWidth: 800,
+    margin: "auto",
+    maxHeight: 780,
+    overflowY: "auto",
+    marginTop: 70,
+  },
+  searchContainer: {
+    height: 150,
+  },
+  searchWraper: {
+    maxWidth: 385,
+    maxHeight: 67,
+    minWidth: 330,
+    border: "3px solid #227B3C",
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 57,
+    borderSizing: "border-box",
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 200,
+    "& .MuiInput-underline:before": {
+      borderColor: "#009688",
     },
-    mainContainer: {
-        marginTop: 30
+    "& .MuiSelect-icon": {
+      top: "calc(50% - 15px)",
     },
-    cardWrap: {
-        maxWidth: 800,
-        margin: 'auto',
-        maxHeight: 780,
-        overflowY: 'auto',
-        marginTop: 70
+    "& .MuiNativeSelect-select:focus": {
+      // backgroundColor: '#C4C4C4'
     },
-    searchContainer: {
-        height: 150
+    "& .makeStyles-formControl-5 .MuiNativeSelect-select.MuiNativeSelect-select:focus": {
+      backgroundColor: "red",
     },
-    searchWraper: {
-        maxWidth: 385,
-        maxHeight: 67,
-        minWidth: 330,
-        border: '3px solid #227B3C',
-        borderRadius: 50,
-        justifyContent: 'center',
-        alignItems: "center",
-        marginTop: 57,
-        borderSizing: 'border-box'
+  },
+  inputLabel: {
+    color: "rgba(0, 0, 0, 0.5393)",
+    "&.Mui-focused": {
+      color: "#599C6D",
     },
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 200,
-        "& .MuiInput-underline:before": {
-            borderColor: '#009688'
-        },
-        "& .MuiSelect-icon": {
-            top: 'calc(50% - 15px)'
-        },
-        '& .MuiNativeSelect-select:focus': {
-            // backgroundColor: '#C4C4C4'
-        },
-        '& .makeStyles-formControl-5 .MuiNativeSelect-select.MuiNativeSelect-select:focus': {
-            backgroundColor: 'red'
-        }
-    },
-    inputLabel: {
-        color: "rgba(0, 0, 0, 0.5393)",
-        "&.Mui-focused": {
-            color: "#599C6D"
-        }
-    },
-    searchIcon: {
-        width: 35,
-        height: 35,
-        backgroundColor: '#009688',
-        padding: 7,
-        borderRadius: '50%',
-        color: '#fff',
-        marginLeft: 58
-    },
-}))
+  },
+  searchIcon: {
+    width: 35,
+    height: 35,
+    backgroundColor: "#009688",
+    padding: 7,
+    borderRadius: "50%",
+    color: "#fff",
+    marginLeft: 58,
+  },
+}));
 
 function sortByCreatedDate(data) {
-    data.sort((a, b) => {
-        if (a.created > b.created) { return 1; }
-        if (a.created < b.created) { return -1; }
-        return 0;
-    })
-
+  data.sort((a, b) => {
+    if (a.created > b.created) {
+      return 1;
+    }
+    if (a.created < b.created) {
+      return -1;
+    }
+    return 0;
+  });
 }
 function Dashboard(props) {
-    const classes = useStyles();
-    const [proposalList, setProposalList] = useState([]);
-    const authContext = ConsumeAuth();
-    const history = useHistory();
-    const [selectedProposals, setSelectedProposals] = useState([]);
-    const [status, setStatus] = useState('')
-    const [selected, setSelected] = useState({});
+  const classes = useStyles();
+  const [proposalList, setProposalList] = useState([]);
+  const authContext = ConsumeAuth();
+  const history = useHistory();
+  const [selectedProposals, setSelectedProposals] = useState([]);
+  const [status, setStatus] = useState("");
+  const [selected, setSelected] = useState({});
 
-    const handleChange = (event) => {
-        let value = event.target.value;
-        setStatus(value);
-        let searchedProposals;
-        if (value === "all") {
-            searchedProposals = proposalList;
-            sortByCreatedDate(searchedProposals);
-        } else if (value === "my") {
-            searchedProposals = proposalList.filter(item => item.creator === authContext.civic.account.accountName);
-            sortByCreatedDate(searchedProposals);
-        } else if (value === ProposalStatus.Approved.toString()) {
-            searchedProposals = proposalList.filter(item => item.status.toString() === value);
-            searchedProposals.sort((a, b) => b.yesVoteCount - a.yesVoteCount);
-        }
-        else {
-            searchedProposals = proposalList.filter(item => item.status.toString() === value);
-            sortByCreatedDate(searchedProposals);
-        }
-        setSelectedProposals(searchedProposals);
-    };
+  const handleChange = (event) => {
+    let value = event.target.value;
+    setStatus(value);
+    let searchedProposals;
+    if (value === "all") {
+      searchedProposals = proposalList;
+      sortByCreatedDate(searchedProposals);
+    } else if (value === "my") {
+      searchedProposals = proposalList.filter(
+        (item) => item.creator === authContext.civic.account.accountName
+      );
+      sortByCreatedDate(searchedProposals);
+    } else if (value === ProposalStatus.Approved.toString()) {
+      searchedProposals = proposalList.filter(
+        (item) => item.status.toString() === value
+      );
+      searchedProposals.sort((a, b) => b.yesVoteCount - a.yesVoteCount);
+    } else {
+      searchedProposals = proposalList.filter(
+        (item) => item.status.toString() === value
+      );
+      sortByCreatedDate(searchedProposals);
+    }
+    setSelectedProposals(searchedProposals);
+  };
 
-    useEffect(() => {
-        async function main() {
-            if (!await authContext.isLoggedIn()) {
-                history.push('/login');
-                return;
-            }
-            let proposals = await authContext.civic.proposalList();
-            proposals = proposals.map(item => {
-                item.position = parseLocation(item.location)
-                return item;
-            });
-            setProposalList(proposals);
-            setSelectedProposals(proposals);
-        }
-
-        main();
-    }, [authContext, history]);
-
-    const navigateToProposal = (proposalId) => {
-        history.push(`/proposal/${proposalId}`);
+  useEffect(() => {
+    async function main() {
+      if (!(await authContext.isLoggedIn())) {
+        history.push("/login");
+        return;
+      }
+      let proposals = await authContext.civic.proposalList();
+      proposals = proposals.map((item) => {
+        item.position = parseLocation(item.location);
+        return item;
+      });
+      setProposalList(proposals);
+      setSelectedProposals(proposals);
     }
 
-    return (
-        <div className={classes.root}>
-            <Grid container direction="column">
+    main();
+  }, [authContext, history]);
+
+  const navigateToProposal = (proposalId) => {
+    history.push(`/proposal/${proposalId}`);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Grid container direction="column">
+        <Grid item>
+          <Navbar />
+        </Grid>
+        <Grid item container className={classes.mainContainer}>
+          <Grid item container xs={6}>
+            <Grid
+              item
+              container
+              alignItems="center"
+              justify="center"
+              className={classes.searchContainer}
+            >
+              <Grid item container className={classes.searchWraper}>
                 <Grid item>
-                    <Navbar />
+                  <FormControl className={classes.formControl}>
+                    <InputLabel
+                      id="search-proposal"
+                      className={classes.inputLabel}
+                    >
+                      Status
+                    </InputLabel>
+                    <NativeSelect
+                      value={status}
+                      onChange={handleChange}
+                      id="search-proposal"
+                    >
+                      <option value="all">All proposals</option>
+                      <option value="my">My proposals</option>
+                      <option value={ProposalStatus.Proposed}>Proposed</option>
+                      <option value={ProposalStatus.Reviewing}>
+                        Reviewing by gov
+                      </option>
+                      <option value={ProposalStatus.Rejected}>
+                        Rejected by gov
+                      </option>
+                      <option value={ProposalStatus.Approved}>Voting</option>
+                      <option value={ProposalStatus.Actioned}>
+                        Executing by gov
+                      </option>
+                      <option value={ProposalStatus.Closed}>Closed</option>
+                    </NativeSelect>
+                  </FormControl>
                 </Grid>
-                <Grid item container className={classes.mainContainer}>
-                    <Grid item container xs={6}>
-                        <Grid item container alignItems="center" justify="center" className={classes.searchContainer}>
-                            <Grid item container className={classes.searchWraper}>
-                                <Grid item>
-                                    <FormControl className={classes.formControl}>
-                                        <InputLabel id="search-proposal" className={classes.inputLabel}>Status</InputLabel>
-                                        <NativeSelect
-                                            value={status}
-                                            onChange={handleChange}
-                                            id="search-proposal"
-                                        >
-                                            <option value="all">All proposals</option>
-                                            <option value="my">My proposals</option>
-                                            <option value={ProposalStatus.Proposed}>Proposed</option>
-                                            <option value={ProposalStatus.Reviewing}>Reviewing by gov</option>
-                                            <option value={ProposalStatus.Rejected}>Rejected by gov</option>
-                                            <option value={ProposalStatus.Approved}>Voting</option>
-                                            <option value={ProposalStatus.Actioned}>Executing by gov</option>
-                                            <option value={ProposalStatus.Closed}>Closed</option>
-                                        </NativeSelect>
-                                    </FormControl>
-                                </Grid>
-                                <Grid item className={classes.searchIcon}>
-                                    <SearchIcon />
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                        {selectedProposals.length === 0 &&
-                            <Grid item container className={classes.cardWrap}>
-                                <div style={{ margin: 'auto' }} > No proposal were found</div>
-                            </Grid>
-                        }
-                        {selectedProposals.length > 0 &&
-                            <Grid item container className={classes.cardWrap}>
-                                {selectedProposals.map(proposal =>
-                                    <Grid item xs={6} key={proposal.proposalId}>
-                                        <Card
-                                            title={proposal.title}
-                                            description={proposal.description}
-                                            imageUrl={proposal.photo}
-                                            selected={selected.proposalId === proposal.proposalId}
-                                            onClick={() => setSelected(proposal)}
-                                            onButtonClick={() => navigateToProposal(proposal.proposalId)}
-                                        />
-                                    </Grid>
-                                )}
-                            </Grid>
-                        }
-                    </Grid>
-                    <Grid item container xs={6}>
-                        <Map selected={selected} onSelect={(item) => setSelected(item)} selectedProposals={selectedProposals} zoom={15} />
-                    </Grid>
+                <Grid item className={classes.searchIcon}>
+                  <SearchIcon />
                 </Grid>
+              </Grid>
             </Grid>
-        </div >
-    );
+            {selectedProposals.length === 0 && (
+              <Grid item container className={classes.cardWrap}>
+                <div style={{ margin: "auto" }}> No proposal were found</div>
+              </Grid>
+            )}
+            {selectedProposals.length > 0 && (
+              <Grid item container className={classes.cardWrap}>
+                {selectedProposals.map((proposal) => (
+                  <Grid item xs={6} key={proposal.proposalId}>
+                    <Card
+                      title={proposal.title}
+                      description={proposal.description}
+                      imageUrl={proposal.photo}
+                      selected={selected.proposalId === proposal.proposalId}
+                      onClick={() => setSelected(proposal)}
+                      onButtonClick={() =>
+                        navigateToProposal(proposal.proposalId)
+                      }
+                      hasBadge={status == ProposalStatus.Approved}
+                      badgeContent={
+                        proposal.yesVoteCount > 0
+                          ? `${proposal.yesVoteCount} ${
+                              proposal.yesVoteCount > 1 ? "votes" : "vote"
+                            }`
+                          : ''
+                      }
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+            )}
+          </Grid>
+          <Grid item container xs={6}>
+            <Map
+              selected={selected}
+              onSelect={(item) => setSelected(item)}
+              selectedProposals={selectedProposals}
+              zoom={15}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Dashboard;
