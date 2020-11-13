c701563c11cfb927432c111cde999bf12b16d9a1
JC Melati <hello@jcmelati.com>
2020-11-13 19:41:50 +0100

    Install axios

--- back-end/integrations/overhd.js
+++ back-end/integrations/overhd.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const categoriesParser = require("../types/proposals/categories");
 const typeParser = require("../types/proposals/type");
 const { hashToBase64 } = require('../utils/base64Utils');
@@ -6,7 +7,7 @@ async function notifyNewProposal(blockchainRes) {
   const data = blockchainRes.processed?.action_traces[0]?.act?.data
   const photo = await hashToBase64(data.photo)
 
-  console.log({
+  const postData = {
     proposal_id: blockchainRes.processed.id,
     creator: data.creator,
     title: data.title,
@@ -17,7 +18,11 @@ async function notifyNewProposal(blockchainRes) {
     location: data.location,
     status: 'Proposed',
     photo
-  });
+  };
+  console.log(postData)
+
+  const request = await axios.post('https://kafka-api.odyssy.nl/proposal', postData)
+  console.log(request)
 }
 
 module.exports = {
