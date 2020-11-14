ee834383f2d1d3f289e8bb3ce20dba68f50ee341
JC Melati <hello@jcmelati.com>
2020-11-14 15:52:43 +0100

    Only send data to OverHd in production

--- back-end/integrations/overhd.js
+++ back-end/integrations/overhd.js
@@ -4,27 +4,29 @@ const axios = require('axios');
 // const { hashToBase64 } = require('../utils/base64Utils');
 
 async function notifyNewProposal(blockchainRes) {
-  const data = blockchainRes.processed?.action_traces[0]?.act?.data;
-  // const photo = await hashToBase64(data.photo);
+  if (process.env.REACT_APP_NODE_ENV === 'production') {
+    const data = blockchainRes.processed?.action_traces[0]?.act?.data;
+    // const photo = await hashToBase64(data.photo);
 
-  const postData = {
-    // proposal_id: blockchainRes.processed.id,
-    // creator: data.creator,
-    // title: data.title,
-    // description: data.description,
-    // category: await categoriesParser.toLabel(data.category),
-    // type: await typeParser.toLabel(data.type),
-    // budget: data.budget,
-    // location: data.location,
-    // status: 'Proposed',
-    // photo,
-    name: data.title,
-    businessTypeName: 'Eerste hulp', // only one type for now
-    requestedFunding: Math.floor(+data.budget), // optional, will default to the game cost of the business type
-    openVoting: true, // optional, default true
-  };
+    const postData = {
+      // proposal_id: blockchainRes.processed.id,
+      // creator: data.creator,
+      // title: data.title,
+      // description: data.description,
+      // category: await categoriesParser.toLabel(data.category),
+      // type: await typeParser.toLabel(data.type),
+      // budget: data.budget,
+      // location: data.location,
+      // status: 'Proposed',
+      // photo,
+      name: data.title,
+      businessTypeName: 'Eerste hulp', // only one type for now
+      requestedFunding: Math.floor(+data.budget), // optional, will default to the game cost of the business type
+      openVoting: true, // optional, default true
+    };
 
-  axios.post('https://kafka-api.odyssy.nl/stake/proposals', postData);
+    axios.post('https://kafka-api.odyssy.nl/stake/proposals', postData);
+  }
 }
 
 module.exports = {
