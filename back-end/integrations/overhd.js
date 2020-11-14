46821233639583f277f855fd946a7e30c33d711a
JC Melati <hello@jcmelati.com>
2020-11-14 15:07:53 +0100

    Send data to OverHd when Proposal is created

--- back-end/integrations/overhd.js
+++ back-end/integrations/overhd.js
@@ -1,28 +1,30 @@
-const axios = require("axios");
-const categoriesParser = require("../types/proposals/categories");
-const typeParser = require("../types/proposals/type");
-const { hashToBase64 } = require('../utils/base64Utils');
+const axios = require('axios');
+// const categoriesParser = require('../types/proposals/categories');
+// const typeParser = require('../types/proposals/type');
+// const { hashToBase64 } = require('../utils/base64Utils');
 
 async function notifyNewProposal(blockchainRes) {
-  const data = blockchainRes.processed?.action_traces[0]?.act?.data
-  const photo = await hashToBase64(data.photo)
+  const data = blockchainRes.processed?.action_traces[0]?.act?.data;
+  // const photo = await hashToBase64(data.photo);
 
   const postData = {
-    proposal_id: blockchainRes.processed.id,
-    creator: data.creator,
-    title: data.title,
-    description: data.description,
-    category: await categoriesParser.toLabel(data.category),
-    type: await typeParser.toLabel(data.type),
-    budget: data.budget,
-    location: data.location,
-    status: 'Proposed',
-    photo
+    // proposal_id: blockchainRes.processed.id,
+    // creator: data.creator,
+    // title: data.title,
+    // description: data.description,
+    // category: await categoriesParser.toLabel(data.category),
+    // type: await typeParser.toLabel(data.type),
+    // budget: data.budget,
+    // location: data.location,
+    // status: 'Proposed',
+    // photo,
+    name: data.title,
+    businessTypeName: 'Eerste hulp', // only one type for now
+    requestedFunding: Math.floor(+data.budget), // optional, will default to the game cost of the business type
+    openVoting: true, // optional, default true
   };
-  console.log(postData)
 
-  const request = await axios.post('https://kafka-api.odyssy.nl/proposal', postData)
-  console.log(request)
+  axios.post('https://kafka-api.odyssy.nl/stake/proposals', postData);
 }
 
 module.exports = {
