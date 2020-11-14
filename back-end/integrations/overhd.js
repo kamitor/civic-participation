73f805d06347693e9b300abaa8373ed432f7a073
JC Melati <hello@jcmelati.com>
2020-11-14 16:48:58 +0100

    Add different sprites on OverHd integration

--- back-end/integrations/overhd.js
+++ back-end/integrations/overhd.js
@@ -3,6 +3,38 @@ const axios = require('axios');
 // const typeParser = require('../types/proposals/type');
 // const { hashToBase64 } = require('../utils/base64Utils');
 
+const categoryIdToOverhdStructure = {
+    0: {
+        label: "Crypto Art Auction" // Green Space
+    },
+    1: {
+        label: "Crypto Art Auction" // Kids
+    },
+    2: {
+        label: "Eerste hulp" // Safety 
+    },
+    3: {
+        label: "Geldautomaat" // Accessibility
+    },
+    4: {
+        label: "Crypto Art Auction" // Art
+    },
+    5: {
+        label: "Eerste hulp" // Health
+    },
+    6: {
+        label: "Geldautomaat" // Road
+    },
+    7: {
+        label: "Crypto Art Auction" // Residential
+    }
+}
+
+async function toOverhdStructure(type) {
+    return categoryIdToOverhdStructure[type].label;
+}
+
+
 async function notifyNewProposal(blockchainRes) {
   if (process.env.REACT_APP_NODE_ENV === 'production') {
     const data = blockchainRes.processed?.action_traces[0]?.act?.data;
@@ -20,7 +52,7 @@ async function notifyNewProposal(blockchainRes) {
       // status: 'Proposed',
       // photo,
       name: data.title,
-      businessTypeName: 'Eerste hulp', // only one type for now
+      businessTypeName: toOverhdStructure(data.category), // only one type for now
       requestedFunding: Math.floor(+data.budget), // optional, will default to the game cost of the business type
       openVoting: true, // optional, default true
     };
